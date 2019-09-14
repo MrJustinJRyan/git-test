8447d6cedca465248358f9d80f0dee36c9c2c0ea
Justin Ryan <jjryan@gmail.com>
2019-09-13 18:11:42 -0700

    core setup and sort js

---
+++ sortthreenumbers.js
@@ -0,0 +1,36 @@
+var x= 0;
+var y=-1;
+var z= 4;
+if (x>y && x>z)
+{
+        if (y>z)
+        {
+            window.alert(x + ", " + y + ", " +z);
+        }
+        else
+        {
+            window.alert(x + ", " + z + ", " +y);
+        }
+}
+else if (y>x && y >z)
+{
+        if (x>z)
+        {
+             window.alert(y + ", " + x + ", " +z);
+        }
+        else
+        {
+             window.alert(y + ", " + z + ", " +x);
+        }
+}
+else if (z>x && z>y)
+{
+        if (x>y)
+        {
+            window.alert(z + ", " + x + ", " +y);
+        }
+        else
+        {
+            window.alert(z + ", " + y + ", " +x);
+        }
+}        
